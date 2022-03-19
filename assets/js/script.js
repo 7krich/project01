82978dab6387ab670f2f17ffbcc7c2ac9b377b52
Kyle Richnafsky <7krich@gmail.com>
2022-03-19 14:11:29 -0400

    random recipe via search now renders on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var userInputEl = document.querySelector("#search");
 var userFormEl = document.querySelector("#user-input-container");
 var userSearchEl = document.querySelector("#user-search-button");
 var selectedRecipeEl = document.querySelector("#recipe-selected");
+var dishNameEl = document.querySelector(".dish-name");
+var ingredientTitleEl = document.querySelector(".ingredient-title");
+var ingredientListEl = document.querySelector(".ingredient-list");
+var instructionsEl = document.querySelector(".instructions");
 
 // // EVENT LISTENERS
 userFormEl.addEventListener("submit", formSubmitHandler);
@@ -23,7 +27,8 @@ function formSubmitHandler(event) {
         userInputEl.value = "";
     } else {
 
-        // THIS NEEDS TO BE A MODAL
+        // ** NEED TO TURN INTO MODAL
+
         alert("Please enter a cuisine");
     }
 };
@@ -48,6 +53,9 @@ function getMeal(CuisineName) {
             alert("Error: " + response.statusText);
         }
     })
+    
+    // ** NEED TO TURN INTO MODAL
+
     // provide user info if server can't be reached
     .catch(function(error) {
         alert("Unable to connect to Server");
@@ -57,6 +65,7 @@ function getMeal(CuisineName) {
 // CONVERT MEAL DATA ARRAY TO IDMEAL TO PASS THROUGH GETRECIPE TO ACCESS ADDTL RECIPE INFO
 function mealToId (data) {
         if (data) {
+            // randomize recipes recieved based on cuisine
             let idMeal = data.meals[Math.floor(Math.random() * data.meals.length)].idMeal;
             getRecipe(idMeal);
             console.log(idMeal);
@@ -100,20 +109,58 @@ function getRecipe (idMeal) {
 
 function displayRecipe (idMeal) {
 
-    //console.log(idMeal.meals[0].strMeal);
-    //for (var i = 0; i < idMeal.meals.length - 1; i++) {
+    console.log(idMeal.meals[0].strMeal);
 
-    //}
-
-    //     if(idMeal) {
-    //         console.log(idMeal.meals[i].strMeal);
-    //     }
-
-    // }
-
-    // if (idMeal) {
-    //     selectedRecipeEl.innerHTML = "";
-    //     selectedRecipeEl.append(meals);
-    // }
+    if (idMeal) {
+        // CLEAR OLD DATA UPON NEW SEARCH
+        dishNameEl.innerHTML = "";
+        ingredientTitleEl.innerHTML = "";
+        ingredientListEl.innerHTML = "";
+        // APPEND DISH NAME
+        dishNameEl.append(idMeal.meals[0].strMeal);
+        // APPEND INGREDIENTS TITLE
+        ingredientTitleEl.append("Ingredients: ");
+        // APPEND INGREDIENTS & PAIR WITH MEASUREMENT
+        ingredientListEl.append(idMeal.meals[0].strIngredient1);
+        ingredientListEl.append(idMeal.meals[0].strMeasure1);
+        ingredientListEl.append(idMeal.meals[0].strIngredient2);
+        ingredientListEl.append(idMeal.meals[0].strMeasure2);
+        ingredientListEl.append(idMeal.meals[0].strIngredient3);
+        ingredientListEl.append(idMeal.meals[0].strMeasure3);
+        ingredientListEl.append(idMeal.meals[0].strIngredient4);
+        ingredientListEl.append(idMeal.meals[0].strMeasure4);
+        ingredientListEl.append(idMeal.meals[0].strIngredient5);
+        ingredientListEl.append(idMeal.meals[0].strMeasure5);
+        ingredientListEl.append(idMeal.meals[0].strIngredient6);
+        ingredientListEl.append(idMeal.meals[0].strMeasure6);
+        ingredientListEl.append(idMeal.meals[0].strIngredient7);
+        ingredientListEl.append(idMeal.meals[0].strMeasure7);
+        ingredientListEl.append(idMeal.meals[0].strIngredient8);
+        ingredientListEl.append(idMeal.meals[0].strMeasure8);
+        ingredientListEl.append(idMeal.meals[0].strIngredient9);
+        ingredientListEl.append(idMeal.meals[0].strMeasure9);
+        ingredientListEl.append(idMeal.meals[0].strIngredient10);
+        ingredientListEl.append(idMeal.meals[0].strMeasure10);
+        ingredientListEl.append(idMeal.meals[0].strIngredient11);
+        ingredientListEl.append(idMeal.meals[0].strMeasure11);
+        ingredientListEl.append(idMeal.meals[0].strIngredient12);
+        ingredientListEl.append(idMeal.meals[0].strMeasure12);
+        ingredientListEl.append(idMeal.meals[0].strIngredient13);
+        ingredientListEl.append(idMeal.meals[0].strMeasure13);
+        ingredientListEl.append(idMeal.meals[0].strIngredient14);
+        ingredientListEl.append(idMeal.meals[0].strMeasure14);
+        ingredientListEl.append(idMeal.meals[0].strIngredient15);
+        ingredientListEl.append(idMeal.meals[0].strMeasure16);
+        ingredientListEl.append(idMeal.meals[0].strIngredient17);
+        ingredientListEl.append(idMeal.meals[0].strMeasure17);
+        ingredientListEl.append(idMeal.meals[0].strIngredient18);
+        ingredientListEl.append(idMeal.meals[0].strMeasure18);
+        ingredientListEl.append(idMeal.meals[0].strIngredient19);
+        ingredientListEl.append(idMeal.meals[0].strMeasure19);
+        ingredientListEl.append(idMeal.meals[0].strIngredient20);
+        ingredientListEl.append(idMeal.meals[0].strMeasure20);
+        // APPEND DIRECTIONS
+        instructionsEl.append(idMeal.meals[0].strInstructions);
+    }
 };
 
