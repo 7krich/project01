5f3656b46fe8332790b06d90debf06241dfd011b
Kyle Richnafsky <7krich@gmail.com>
2022-03-19 10:19:07 -0400

    search by cuisine & transfer to ID for addtl info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-// DEFINE VARIABLES
-
+// // DEFINE VARIABLES
 var userInputEl = document.querySelector("#search");
-var userFormEl = document.querySelector("#user-input-container")
-var userSearchEl = document.querySelector("#user-search-button")
-// const appId = "c2854cdb";
-// const appKey = "0313cdf2d19bedadc25217aefd97971f";
+var userFormEl = document.querySelector("#user-input-container");
+var userSearchEl = document.querySelector("#user-search-button");
+var selectedRecipeEl = document.querySelector("#recipe-selected");
 
-// EVENT LISTENERS
+// // EVENT LISTENERS
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 // HANDLE SUBMIT EVENT
@@ -18,7 +16,7 @@ function formSubmitHandler(event) {
     var foodType = userInputEl.value;
 
     if (foodType) {
-        getRecipe(foodType);
+        getMeal(foodType);
         //addToSearchHistory();
 
         // clear old content
@@ -26,13 +24,13 @@ function formSubmitHandler(event) {
     } else {
 
         // THIS NEEDS TO BE A MODAL
-        alert("Please enter food type");
+        alert("Please enter a food type");
     }
 };
 
-// GET RECIPE INFO
-function getRecipe(strCategory) {
-    var apiUrl = "https://themealdb.com/api/json/v1/1/search.php?s=" + strCategory;
+// // GET RECIPE INFO
+function getMeal(CuisineName) {
+    var apiUrl = `https://themealdb.com/api/json/v1/1/filter.php?a=${CuisineName}`;
 
     // make get request to URL
     fetch(apiUrl)
@@ -43,7 +41,7 @@ function getRecipe(strCategory) {
             response.json()
             .then(function(data) {
                 console.log(data);
-                //displayRecipe(data);
+                mealToId(data);
             });
             // response recieved but error with request
         } else {
@@ -56,4 +54,38 @@ function getRecipe(strCategory) {
     });
 };
 
+function mealToId (data) {
+    for (var i = 0; i <= data.meals.length; i++) {
 
+        if (data) {
+            let idMeal = data.meals[i].idMeal;
+            getRecipe(idMeal);
+            console.log(idMeal);
+        }
+    }
+};
+
+function getRecipe (idMeal) {
+    var apiUrl = `https://themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+
+    // make get request to URL
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            // display current day in header
+            response.json()
+            .then(function(idMeal) {
+                console.log(idMeal);
+                //displayRecipe(data);
+            });
+            // response recieved but error with request
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    // provide user info if server can't be reached
+    .catch(function(error) {
+        alert("Unable to connect to Server");
+    });
+};
