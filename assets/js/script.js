e839ec42410004bacd9ed5701c8a26853adcb8b2
Kyle Richnafsky <7krich@gmail.com>
2022-03-22 18:58:03 -0400

    working on bugs in ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -544,34 +544,35 @@ function displaySavedRecipe (data) {
         // APPEND INGREDIENTS & PAIR WITH MEASUREMENT
 
         let ingredientItem = [
-            data.meals[0].strIngredient1, data.meals[0].strIngredient2, data.meals[0].strIngredient3, data.meals[0].strIngredient4, data.meals[0].strIngredient5,data.meals[0].strIngredient6, data.meals[0].strIngredient7, data.meals[0].strIngredient8, data.meals[0].strIngredient9, data.meals[0].strIngredient10, data.meals[0].strIngredient11, data.meals[0].strIngredient12, data.meals[0].strIngredient13, data.meals[0].strIngredient14, data.meals[0].strIngredient15, data.meals[0].strIngredient16, data.meals[0].strIngredient17, data.meals[0].strIngredient18, data.meals[0].strIngredient19, data.meals[0].strIngredient20]
+            data.meals[0].strIngredient1, data.meals[0].strIngredient2, data.meals[0].strIngredient3, data.meals[0].strIngredient4, data.meals[0].strIngredient5,data.meals[0].strIngredient6, data.meals[0].strIngredient7, data.meals[0].strIngredient8, data.meals[0].strIngredient9, data.meals[0].strIngredient10, data.meals[0].strIngredient11, data.meals[0].strIngredient12, data.meals[0].strIngredient13, data.meals[0].strIngredient14, data.meals[0].strIngredient15, data.meals[0].strIngredient16, data.meals[0].strIngredient17, data.meals[0].strIngredient18, data.meals[0].strIngredient19, data.meals[0].strIngredient20
+        ]
 
         let ingredientMeasure = [
             data.meals[0].strMeasure1, data.meals[0].strMeasure2, data.meals[0].strMeasure3, data.meals[0].strMeasure4, data.meals[0].strMeasure5, data.meals[0].strMeasure6, data.meals[0].strMeasure7, data.meals[0].strMeasure8, data.meals[0].strMeasure9, data.meals[0].strMeasure10, data.meals[0].strMeasure11, data.meals[0].strMeasure12, data.meals[0].strMeasure13, data.meals[0].strMeasure14, data.meals[0].strMeasure15, data.meals[0].strMeasure16, data.meals[0].strMeasure17, data.meals[0].strMeasure18, data.meals[0].strMeasure19, data.meals[0].strMeasure20,
         ]
 
-            if (ingredientItem !== "null" || ingredientItem !== "") {
-
             for (var i = 0; i <= ingredientItem.length; i++) {
-                
-                let li = document.createElement("li");
-                li.innerText = ingredientMeasure[i];
-                document.body.appendChild(li);
-            }
-        };
 
-        if (ingredientMeasure !== "null" || ingredientMeasure !== "") {
+                if (ingredientItem[i]) {
 
-            for (var i = 0; i < ingredientMeasure.length; i++) {
-
-                let li = document.createElement("li");
-                li.innerText = ingredientMeasure[i];
-                document.body.appendChild(li);
+                    let li = document.createElement("li");
+                    li.innerText = ingredientItem[i];
+                    document.body.appendChild(li);
+                }
             };
 
-        } else {
-            li.innerText = ""
-        }
+        // if (ingredientMeasure !== null || ingredientMeasure !== "") {
+
+        //     for (var i = 0; i < ingredientMeasure.length; i++) {
+
+        //         let li = document.createElement("li");
+        //         li.innerText = ingredientMeasure[i];
+        //         document.body.appendChild(li);
+        //     };
+
+        // } else {
+        //     li.innerText = ""
+        // }
 
         // ingredientListHistEl.append(data.meals[0].strIngredient1);
         // ingredientListHistEl.append(data.meals[0].strMeasure1);
